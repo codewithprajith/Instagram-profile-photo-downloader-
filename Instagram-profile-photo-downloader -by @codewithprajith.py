9003ff7ee85ed0c053e35dabf1c957f91872d773
Code With Prajith <codewithprajith@gmail.com>
2022-02-15 14:59:55 +0530

    Update Instagram-profile-photo-downloader -by @codewithprajith.py

--- Instagram-profile-photo-downloader -by @codewithprajith.py
+++ Instagram-profile-photo-downloader -by @codewithprajith.py
@@ -1,5 +1,8 @@
+#follow @codewithprajith for more projects
 import instaloader
 
 app = instaloader.Instaloader()
 user_name = input("User name:")
 app.download_profile(user_name, profile_pic_only=True)
+
+#follow @codewithprajith for more projects
